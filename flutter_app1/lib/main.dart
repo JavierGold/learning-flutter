820d93de84b72426a35346468d312a111b14fc38
JavierGold <567498jav@gmail.com>
2021-09-28 17:15:28 -0500

    add color

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -4,14 +4,26 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('mi primer app'),
+        title: Text(
+            'mi primer app'
+        ),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('hola chavos'),
+        child: Text(''
+            'hola chavos',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text('click') ,
+          backgroundColor: Colors.red[600] ,
           onPressed: () {}
       ),
     ),
