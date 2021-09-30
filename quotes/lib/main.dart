bcfc1b7c3cd47fdf26b033e3f626344e2d25905c
JavierGold <567498jav@gmail.com>
2021-09-30 17:59:44 -0500

    Poner lista en el body con StatefulWidget

---
+++ quotes/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes =[
+    "Quote numero 1",
+    "Quote numero 2",
+    "Quote numero 3"
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote){
+          return Text(quote);
+        }).toList(),
+      ),
+    );
+  }
+}
+
