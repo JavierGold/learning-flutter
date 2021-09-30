d9e437fb29416a0ab045c0fa174237ac1a7d01ff
JavierGold <567498jav@gmail.com>
2021-09-30 16:24:18 -0500

    add current level

--- id_app/lib/main.dart
+++ id_app/lib/main.dart
@@ -27,6 +27,7 @@ class NinjaCard extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
+            SizedBox(height: 10.0),
             Text(
               'Chun-Li',
               style: TextStyle(
@@ -34,6 +35,24 @@ class NinjaCard extends StatelessWidget {
                 letterSpacing: 2.0,
                 fontSize: 28.0,
                 fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color:Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                  color:Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
               ),
             ),
           ],
